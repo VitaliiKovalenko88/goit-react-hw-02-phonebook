2343f083b7bdc582ae5c59fd034c9810de2060c7
VitaliiKovalenko88 <vintilyator88@gmail.com>
2022-01-12 17:37:17 +0100

    Add settings

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
   nameInputId = () => nanoid();
 
@@ -49,6 +50,15 @@ export class App extends Component {
             name="name"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handleInputChange}
+          />
+          <label>Number</label>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             onChange={this.handleInputChange}
           />
