f9edb89f9fea2f156996ffdfe5ab2bc6f95099e0
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-12 22:11:28 +0100

    comlete task 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import './App.css';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
 
-    </div>
-  );
+  generateId = () => nanoid();
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { value } = e.target.elements.name;
+
+    this.setState(prevState => ({
+      contacts: [value, ...prevState.contacts],
+    }));
+    this.reset();
+    e.currentTarget.reset();
+  };
+
+  handleChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ name: value });
+  };
+
+  reset = () => {
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <section>
+          <h2>PhoneBook</h2>
+          <form onSubmit={this.handleSubmit}>
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              required
+            />
+            <button type="submit">Add contact</button>
+          </form>
+        </section>
+        <section>
+          <h2>Contacts</h2>
+          <ul>
+            {this.state.contacts.map(contact => {
+              return <li key={this.generateId()}>{contact}</li>;
+            })}
+          </ul>
+        </section>
+      </div>
+    );
+  }
 }
-
-export default App;
