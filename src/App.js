48af98917fd19e74715396b9ec298b076677eb4b
VitaliiKovalenko88 <vintilyator88@gmail.com>
2023-11-14 21:03:10 +0100

    comlete task 3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Form from './Comonents/Form/Form';
 
 export default class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   generateId = () => nanoid();
@@ -17,13 +23,25 @@ export default class App extends Component {
     };
 
     this.setState(prevState => {
-      console.log({
-        contacts: [dataContacts, ...prevState.contacts],
-      });
       return {
         contacts: [dataContacts, ...prevState.contacts],
       };
     });
+  };
+
+  onChangeFilter = e => {
+    const { value } = e.currentTarget;
+    this.setState({ filter: value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
   };
 
   render() {
@@ -35,8 +53,14 @@ export default class App extends Component {
         </section>
         <section>
           <h2>Contacts</h2>
+          <p>Find contacts by name</p>
+          <input
+            type="text"
+            value={this.state.filter}
+            onChange={this.onChangeFilter}
+          />
           <ul>
-            {this.state.contacts.map(contact => {
+            {this.getVisibleContacts().map(contact => {
               return (
                 <li key={contact.id}>
                   {contact.name}: {contact.number}
--- src/Comonents/Form/Form.jsx
+++ src/Comonents/Form/Form.jsx
@@ -1,60 +1,61 @@
 import { Component } from 'react';
 
 export default class Form extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+	state = {
+		name: '',
+		number: '',
+	};
 
-  handleChange = e => {
-    const { value, name } = e.currentTarget;
+	handleChange = e => {
+		const { value, name } = e.currentTarget;
 
-    this.setState({ [name]: value });
-  };
+		this.setState({ [name]: value });
+	};
 
-  handleSubmit = e => {
-    e.preventDefault();
+	handleSubmit = e => {
+		e.preventDefault();
 
-    const { value } = e.currentTarget;
-    const { name, number } = this.state;
-    this.props.onSubmit(name, value, number);
+		const { value } = e.currentTarget;
+		const { name, number } = this.state;
+		this.props.onSubmit(name, value, number);
 
-    this.reset();
-    e.currentTarget.reset();
-  };
+		this.reset();
+		e.currentTarget.reset();
+	};
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
+	reset = () => {
+		this.setState({ name: '', number: '' });
+	};
 
-  render() {
-    return (
-      <>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+	render() {
+		return (
+			<>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Name
+						<input
+							type="text"
+							name="name"
+							value={this.state.name}
+							onChange={this.handleChange}
+							required
+						/>
+					</label>
 
-          <label>
-            Number
-            <input
-              type="tel"
-              name="number"
-              value={this.state.number}
-              onChange={this.handleChange}
-              required
-            />
-          </label>
+					<label>
+						Number
+						<input
+							type="tel"
+							name="number"
+							value={this.state.number}
+							onChange={this.handleChange}
+							required
+						/>
+					</label>
 
-          <button type="submit">Add contact</button>
-        </form>
-      </>
-    );
-  }
+					<button type="submit">Add contact</button>
+				</form>
+			</>
+		);
+	}
 }
